4a037dbe710d54bdfe046afca9d911e8aaac45a2
Petr Baca <pb.workspace@gmail.com>
2017-02-26 18:14:29 +0100

    demo done

--- demo_0-0.R
+++ demo_0-0.R
@@ -44,5 +44,3 @@ for (i in 1:ncol(dat)) {
 title(main = "Oil production (mbpd)", outer = TRUE, adj = 0.01, cex.main = 1.6)
 title(sub = "Source: EIA (via Quandl), CSOB", outer = TRUE, line = 0, adj = 1)
 ## end
-
-## test test
